c5ca10c518e62aa1c0579fc483c00ab44b4a5f04
Karan Asthana <karan.asthana1@gmail.com>
2021-10-10 23:44:35 -0400

    renamed files

--- analyse.py
+++ analyse.py
@@ -16,27 +16,12 @@ def readCSVFile():
     rows = []
 
     # with open("data.csv", 'r') as file:
-    with open("/Users/karanasthana/Documents/UFL/AOA/Assigments/Assignment/data.csv", 'r') as file:
+    with open("data.csv", 'r') as file:
         csvreader = csv.reader(file)
         for row in csvreader:
             rows.append(row)
     header = ['num_vertices', 'num_edges', 'runtime'];
     return header, rows
-
-def prettyPrintPrezList(header, list):
-    fig, ax = plt.subplots()
-
-    # hide axes
-    fig.patch.set_visible(False)
-    ax.axis('off')
-    ax.axis('tight')
-
-    list.insert(0, header)
-    df = pandas.DataFrame(list[1:11], columns=header)
-
-    ax.table(cellText=df.values, colLabels=df.columns, colColours=(['lightblue'] * len(header)))
-    fig.tight_layout()
-    plt.show()
 
 def showDataOnGraph(rows):
     # tlist = list(zip(*rows))
@@ -44,10 +29,7 @@ def showDataOnGraph(rows):
     for row in rows:
         if ((int)(row[2]) < 10000):
             continue
-        plt.scatter((int)(row[0]), (int)(row[2]))
-
-
-    # computations to fetch the values of statistical data
+        plt.scatter((int)(row[0]), (int)(row[2]), color='blue')
 
     plt.show()
 
